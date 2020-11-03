3868b6e6b79f740924c52374dc6fa7531d8d11c2
Robert Rozas Navarro <rorozasn@microsoft.com>
2020-11-03 08:39:59 -0300

    dia 2

---
+++ ConferencePlanner/BackEnd/Data/Speaker.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System;
+using System.Collections.Generic;
+
+namespace BackEnd.Data
+{
+    public class Speaker : ConferenceDTO.Speaker
+    {
+        public virtual ICollection<SessionSpeaker> SessionSpeakers { get; set; } = new List<SessionSpeaker>();
+    }
+}
